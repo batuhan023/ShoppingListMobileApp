149d2d4298cb34270a4f2b05757ad98e6a6121c8
Batuhan <batuhanyavuz467@gmail.com>
2023-08-04 17:09:45 +0300

    04.08.2023

--- Services/LoginService.cs
+++ Services/LoginService.cs
@@ -1,17 +1,21 @@
 ﻿using Azure;
 using EntityLayer.DTOs;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Newtonsoft.Json;
+using Org.Apache.Http.Protocol;
 using ShoppingListMobileApp1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Web.Http;
 
 namespace ShoppingListMobileApp1.Services
 {
@@ -19,9 +23,9 @@ namespace ShoppingListMobileApp1.Services
 
       
     {
-        private const string ApiUrl = "https://192.168.159.1:45456/api/Users/login";
+        private const string ApiUrl = "https://192.168.159.1:45455/api/";
 
-      
+        private readonly HttpClient _httpClient;
 
         public LoginService()
         {
@@ -31,6 +35,7 @@ namespace ShoppingListMobileApp1.Services
             };
 
             _httpClient = new HttpClient(handler);
+            _httpClient.Timeout = TimeSpan.FromSeconds(400);
         }
 
         //public async Task<User> Login(string email, string password)
@@ -58,15 +63,25 @@ namespace ShoppingListMobileApp1.Services
             var dto = new LoginDTO { Email = username, Password = password };
             var content = new StringContent(JsonConvert.SerializeObject(dto), Encoding.UTF8, "application/json");
 
-            var response = await _httpClient.PostAsync(ApiUrl, content);
+            var response = await _httpClient.PostAsync(ApiUrl + "Users/login", content);
             //var response = await _httpClient.PostAsync($"{ApiUrl}Users/login?email={username}&password={password}", content);
             //var response = await _httpClient.PostAsync($"{ApiUrl}Users/Login?email={username}&password={password}");
+            TokenDTO model = new();
 
             if (response.IsSuccessStatusCode)
             {
                 var jsonString = await response.Content.ReadAsStringAsync();
-                var data = JsonConvert.DeserializeObject<List<User>>(jsonString);
-                return data.FirstOrDefault();
+                //var data = JsonConvert.DeserializeObject<List<User>>(jsonString);
+                //var responseData = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+                var user = JsonConvert.DeserializeObject<User>(jsonString);
+                //return data.FirstOrDefault();
+                //if (responseData.ContainsKey("access_token"))
+                //{
+                //    string token = responseData["accsess_token"];
+                // Token'ı kullanmak için yapılacak işlemler
+                // Örneğin, Preferences'e kaydedebilir, diğer API çağrıları için kullanabilirsiniz.
+
+                return user;
             }
             else
             {
